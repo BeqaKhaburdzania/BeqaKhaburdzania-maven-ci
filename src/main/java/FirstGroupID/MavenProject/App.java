33ea3de9589e0424a3a08a694b01c639aba01207
BeqaKhaburdzania <bkhab12@freeuni.edu.ge>
2015-04-26 15:23:07 +0400

    Step Seven

--- src/main/java/FirstGroupID/MavenProject/App.java
+++ src/main/java/FirstGroupID/MavenProject/App.java
@@ -6,8 +6,14 @@ package FirstGroupID.MavenProject;
  */
 public class App 
 {
+	
+	private static void sixStep(){
+		System.out.println("Six Step");
+	}
+	
     public static void main( String[] args )
     {
+    	sixStep();
         System.out.println( "Hello World!" );
     }
 }
