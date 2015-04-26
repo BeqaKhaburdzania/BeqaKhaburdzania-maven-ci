42887640c2df7161e3834af0c43303f794bea0c9
BeqaKhaburdzania <bkhab12@freeuni.edu.ge>
2015-04-26 15:54:05 +0400

    Step 8

--- src/test/java/FirstGroupID/MavenProject/TestSimpleClass.java
+++ src/test/java/FirstGroupID/MavenProject/TestSimpleClass.java
@@ -1,7 +1,8 @@
 package FirstGroupID.MavenProject;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 import org.junit.Test;
 
 public class TestSimpleClass {
@@ -12,4 +13,13 @@ public class TestSimpleClass {
 		int forCompare = curr.GetInt();
 		assertEquals(temp,forCompare);
 	}
+	
+	@Test
+	public void TestWithMock(){
+		int sameResultAlways = 10;
+		SimpleClass temp = mock(SimpleClass.class);
+		when(temp.GetInt()).thenReturn(sameResultAlways);
+		assertEquals(temp.GetInt(),sameResultAlways);
+	}
+
 }
